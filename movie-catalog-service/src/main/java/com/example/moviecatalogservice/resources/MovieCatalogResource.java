6dc119ef638d87c96e899e68009e5807708cce43
sofferor <sofferor@gmail.com>
2021-07-14 16:09:18 +0300

    Just for example - showing how to use the WebClient instead of RestTemplate for services communication.

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,16 +23,37 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         List<String> movieIds = Arrays.asList("id1", "id2");
         List<Rating> ratings = movieIds.stream().map(movieId -> {
-            return restTemplate.getForObject("http://localhost:8083/rating/" + movieId, Rating.class);
+            // return restTemplate.getForObject("http://localhost:8083/rating/" + movieId, Rating.class);
+
+            return webClientBuilder
+                    .build()
+                    .get()
+                    .uri("http://localhost:8083/rating/" + movieId)
+                    .retrieve()
+                    .bodyToMono(Rating.class)
+                    .block();
+
         }).collect(Collectors.toList());
 
         List<CatalogItem> catalogItems = ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+            // Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+
+            Movie movie = webClientBuilder
+                    .build()
+                    .get()
+                    .uri("http://localhost:8082/movie/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+
             return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
         }).collect(Collectors.toList());
 
